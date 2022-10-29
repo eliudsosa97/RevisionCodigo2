2207900f08fed8a00cd56eb25b970d654c9fde5b
eliudsosa97 <113313650+eliudsosa97@users.noreply.github.com>
2022-10-28 22:03:07 -0500

    Add files via upload

---
+++ formulario.js
@@ -0,0 +1,109 @@
+let formulario = document.querySelector(".formulario")
+
+formulario.addEventListener = ("submit", function(fiesta) {
+
+  fiesta.preventDefault();
+  
+let name = formulario.elements[0]
+let age = formulario.elements[1]
+let nacion = formulario.elements[2]
+
+let nombre = name.value
+let edad = age.value
+
+let i = nacion.selectedIndex
+let nacionalidad = nacion.options[i].value
+  console.log(nombre, edad)
+  console.log(nacionalidad)
+
+  if (nombre.length === 0) {
+    name.classList.add("error")
+  }
+  if (edad < 18 || edad > 120) {
+    age.classList.add("error")
+  }
+
+if (nombre.length > 0 
+  && (edad > 18 
+    && edad < 120) ) {
+  agregarInvitado(nombre, edad, nacionalidad)
+  }
+})
+
+let botonBorrar = document.createElement("button")
+botonBorrar.textContent = "Eliminar invitado"
+botonBorrar.id = "boton-borrar"
+let corteLinea = document.createElement("br")
+document.body.appendChild(corteLinea)
+document.body.appendChild(botonBorrar);
+
+function agregarInvitado(nombre, edad, nacionalidad) {
+
+  if (nacionalidad === "ar") {
+    nacionalidad = "Argentina"
+  }
+  else if (nacionalidad === "mx") {
+    nacionalidad = "Mexicana"
+  }
+  else if (nacionalidad === "vnzl") {
+    nacionalidad = "Venezolana"
+  }
+  else if (nacionalidad === "per") {
+    nacionalidad = "Peruana"
+  }
+
+let lista = document.getElementById("Lista-de-invitados")
+
+let elementoLista = document.createElement("div")
+elementoLista.classList.added("elemento-lista")
+lista.appendChild(elementoLista)
+
+let spanNombre = document.createElement("span")
+let inputNombre = document.createElement("input")
+let espacio = document.createElement("br")
+spanNombre.textContent = "Nombre: "
+inputNombre.value = nombre 
+elementoLista.appendChild(spanNombre)
+elementoLista.appendChild(inputNombre)
+elementoLista.appendChild(espacio)
+
+function crearElemento(descripcion, valor) {
+let spanNombre = document.createElement("span")
+let inputNombre = document.createElement("input")
+let espacio = document.createElement("br")
+spanNombre.textContent = descripcion + ": "
+inputNombre.value = valor 
+elementoLista.appendChild(spanNombre)
+elementoLista.appendChild(inputNombre)
+elementoLista.appendChild(espacio)
+}
+
+crearElemento("Nombre", nombre)
+crearElemento("Edad", edad)
+crearElemento("Nacionalidad", nacionalidad)
+
+
+let botonBorrar = document.createElement("button")
+botonBorrar.textContent = "Eliminar invitado"
+botonBorrar.id = "boton-borrar"
+let corteLinea = document.createElement("br")
+elementoLista.appendChild(corteLinea)
+elementoLista.appendChild(botonBorrar);
+
+  botonBorrar.onclick = function() {
+// this.parentNode.style.display = 'none';
+botonBorrar.parentNode.remove()
+  }
+}
+
+/**
+ * Primero sustituí var por let para un mejor control y como buena práctica.
+ * Se cambió el nombre de las variables con una sóla letra por un nombre más entendible
+ * Eliminé una variable que no se estaba utilizando
+ * La variable "e" se confundía con la función, entonce cambié de nombre a la variable
+ * y la función.
+ * Agregué prevent Default
+ * Cambié onsubmit por submit para que apareciera el botón de enviar
+ * Pero aún no funciona );
+ * 
+ */
